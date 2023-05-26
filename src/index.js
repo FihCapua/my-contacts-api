f98a949f7b0db0b4a5351688bf079bd4dbda43cd
FihCapua <fihcapua@gmail.com>
2023-05-25 21:39:45 -0300

    feat: create middlewares for CORS and handleError API

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const router = require('./routes');
+const cors = require('./app/middlewares/cors');
+const handleError = require('./app/middlewares/handleError');
 require('express-async-errors');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors)
 app.use(router);
-app.use((error, request, response, next) => {
-  console.log('### Error handler ###');
-  console.log(error);
-  response.sendStatus(500);
-});
+app.use(handleError);
 
-app.listen(3000, () => console.log('Server is running on http://localhost:3000'));
+app.listen(3001, () => console.log('Server is running on http://localhost:3001'));
